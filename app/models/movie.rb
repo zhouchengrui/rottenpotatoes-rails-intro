4104a9d839006f1608e196a8db77b22c5b3793a4
Chengrui Zhou <cz2664@columbia.edu>
2021-02-05 10:31:28 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
-class Movie < ActiveRecord::Base
-end
+# class Movie < ActiveRecord::Base
+# end
+
+class Movie
+  def self.with_ratings(ratings_list)
+  # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+  #  movies with those ratings
+  # if ratings_list is nil, retrieve ALL movies
+  end
+ end
